21dc7ee6a748f4f73c4b86e881bccb4af8db0ab6
zezking <zhaoenze001@gmail.com>
2021-02-10 19:33:43 +0000

    snake can move now

---
+++ client.js
@@ -0,0 +1,39 @@
+const net = require("net");
+const { setInterval } = require("timers");
+const server = net.createServer();
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: "192.168.1.66",
+    port: 50541,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Game Connected");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: ZEZ");
+    setInterval(() => {
+      conn.write("Move: up");
+      // setTimeout(() => {
+      //   conn.write("Move: left");
+      //   setTimeout(() => {
+      //     conn.write("Move: left");
+      //   }, 600);
+      // }, 550);
+    }, 50);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
